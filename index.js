625d0536cc55db8f279e5d49943a438536094ef4
scanservedelhi-collab <scanserve.delhi@gmail.com>
2025-11-16 00:27:05 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-// src/index.js
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css"; // optional, create if you want basic styles
-
-const root = createRoot(document.getElementById("root"));
-root.render(<App />);
