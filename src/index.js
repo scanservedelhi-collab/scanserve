28423392e43ca21d1a90ee17122bde061774f558
scanservedelhi-collab <scanserve.delhi@gmail.com>
2025-11-16 01:03:38 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./index.css"; // optional, create if you want basic styles
+//import "./index.css"; // optional, create if you want basic styles
 
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
